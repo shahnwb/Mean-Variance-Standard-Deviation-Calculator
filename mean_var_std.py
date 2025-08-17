0755b42ffe6c8034b0b95fba5523281ff48d5ec8
shahnwb <aishahabdulaziz777@gmail.com>
2025-08-17 14:35:15 +0800

    Update mean_var_std.py

--- mean_var_std.py
+++ mean_var_std.py
@@ -11,9 +11,10 @@ def calculate(list):
         calculations = {
             'mean':[mat.mean(axis=0),mat.mean(axis=1),fl.mean()],
             'variance':[mat.var(axis=0),mat.var(axis=1),fl.var()],
-            'standard deviation':[mat.std(axis=0),mat.std(axis=1),fl.std],
+            'standard deviation':[mat.std(axis=0),mat.std(axis=1),fl.std()],
             'max':[mat.max(axis=0),mat.max(axis=1),fl.max()],
             'min':[mat.min(axis=0),mat.min(axis=1),fl.min()],
             'sum':[mat.sum(axis=0),mat.sum(axis=1),fl.sum()]
         }
+
     return calculations
